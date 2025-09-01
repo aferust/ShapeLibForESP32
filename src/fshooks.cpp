39f392594421786e2548b2eda61a4eaf55bb2357
aferust <aferust@gmail.com>
2025-09-01 13:32:52 +0300

    remove !shp_fs->exists(pszFilename)

--- src/fshooks.cpp
+++ src/fshooks.cpp
@@ -6,10 +6,11 @@ SAFile *SADFOpen(const char *pszFilename, const char *pszAccess,
                  void *pvUserData)
 {
     (void)pvUserData;
-	
-	if(!shp_fs->exists(pszFilename)){
-		return nullptr;
-	}
+
+    /*if (strcmp(pszAccess, "w+") == 0 && !shp_fs->exists(pszFilename)) // need this?
+    {
+        return nullptr;
+    }*/
 
     auto file = new fs::File(shp_fs->open(pszFilename, pszAccess));
     if (!(*file))
